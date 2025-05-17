6e95af6b7c5b4f6b98a1fa994f7655b4c01198a6
Shreya Singh <singh.s.shreya2004@gmail.com>
2025-05-17 19:56:42 +0530

    Update InsertionInALinkedList.c

--- LinkedList/InsertionInALinkedList.c
+++ LinkedList/InsertionInALinkedList.c
@@ -64,5 +64,142 @@ int main(){
 //T.C:O(1)
 
 //Insert at the last.
+//insert the value given at tail.
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+ public:
+ int data;
+ Node*next;
+
+ public:
+ Node(int data1, Node*next1){
+   data=data1;
+   next=next1;
+ }
+
+ public:
+ Node(int data1){
+   data=data1;
+   next=nullptr;
+ }
+};
+
+Node*convertArr2LL(vector<int>&arr){
+   Node*head=new Node(arr[0]);
+   Node*mover=head;
+   for(int i=1;i<arr.size();i++){
+      Node*temp=new Node(arr[i]);
+      mover->next=temp;
+      mover=temp;
+   }
+   return head;
+}
+
+void print(Node*head){
+   while(head!=NULL){
+    cout<<head->data<<" ";
+    head=head->next;
+   }
+   cout<<endl;
+}
+
+//whenever we see that temp->next=NULL , we will stop.
+Node*insertTail(Node*head, int val){
+   if(head==NULL){
+      return new Node(val);  //single element is created
+   }
+   //what if it has ..take a temp
+   Node*temp=head;
+   while(temp->next!=NULL){
+      temp=temp->next;
+   }
+   Node* newNode= new Node(val); //pointimg to null 
+   temp->next=newNode;
+   return head;
+}
+int main(){
+   vector<int>arr={12,8,5,7};
+   Node*head=convertArr2LL(arr);
+   head=insertTail(head,100);
+   print(head);
+   return 0;
+}
+
+//insert the value(el) given at kth element.
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+ public:
+ int data;
+ Node*next;
+
+ public:
+ Node(int data1, Node*next1){
+   data=data1;
+   next=next1;
+ }
+
+ public:
+ Node(int data1){
+   data=data1;
+   next=nullptr;
+ }
+};
+
+Node*convertArr2LL(vector<int>&arr){
+   Node*head=new Node(arr[0]);
+   Node*mover=head;
+   for(int i=1;i<arr.size();i++){
+      Node*temp=new Node(arr[i]);
+      mover->next=temp;
+      mover=temp;
+   }
+   return head;
+}
+
+void print(Node*head){
+   while(head!=NULL){
+    cout<<head->data<<" ";
+    head=head->next;
+   }
+   cout<<endl;
+}
+
+Node*insertk(Node*head, int el, int k){
+   if(head==NULL){
+      if(k==1)  return new Node(el);
+      else  return NULL;   
+   }
+   if(k==1){
+      Node*temp= new Node(el, head); //pointing to head
+      return temp;
+   }
+  int cnt=0;
+  Node*temp=head;
+  while(temp!=NULL){
+   cnt++;
+   if(cnt==k-1){
+     Node*x=new Node(el);
+     x->next=temp->next;
+     temp->next=x;
+     break;
+   }
+   temp=temp->next;
+  }
+  return head;
+}
+
+int main(){
+   vector<int>arr={12,8,5,7};
+   Node*head=convertArr2LL(arr);
+   head=insertk(head,100,2);
+   print(head);
+   return 0;
+}
+
+
+
+
 
 
